64c5a09072f91e6d2c7bb4d962674031f391920d
Irtiqa Malik <Irtiqa.m02@gmail.com>
2025-05-19 15:05:41 +0530

    Time: 20 ms (16.52%), Space: 49.1 MB (11.63%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -5,20 +5,19 @@ class Solution {
         HashMap<String,List<String>> hash = new HashMap<>();
 
         for(String str : strs){
-            int[] count = new int[26];
-            for(char s: str.toCharArray()){
-                count[s-'a']++;
+            int[] count = new int[26]; // for 'a' to 'z'
+            for (char c : str.toCharArray()) {
+                count[c - 'a']++;
             }
 
-            // create key from this
-            StringBuilder sb = new StringBuilder("");
-            for(int i = 0 ; i < 26; i++){
-                sb.append(count[i]);
-                sb.append("#");
+            // Convert count array to a string key
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 26; i++) {
+                sb.append(count[i]).append('#'); // add delimiter to prevent overlap
             }
+            String key = sb.toString();
 
-            
-
+        
             // add to hashmap initialisng a new list if required
             hash.putIfAbsent(sb.toString(),new ArrayList<>());
             hash.get(sb.toString()).add(str);
