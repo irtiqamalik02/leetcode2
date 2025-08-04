c3c365d3800489d430431bc03beaf6553cf22b13
Irtiqa Malik <Irtiqa.m02@gmail.com>
2025-08-04 21:08:27 +0530

    Time: 4 ms (94.24%), Space: 17.9 MB (64.32%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,16 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-
-        charFreqMap = {}
-
-        for i in range(len(s)):
-            charFreqMap[s[i]] =  charFreqMap.get(s[i],0)+1
-            charFreqMap[t[i]] =  charFreqMap.get(t[i],0)-1
-
-        for val in charFreqMap.values():
-            if val != 0:
-                return False
-
-        return True            
+        return Counter(s) == Counter(t)
