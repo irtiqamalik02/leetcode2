9a59613b4ee3af347aa9ebb13ac17fe575cdd785
Irtiqa Malik <Irtiqa.m02@gmail.com>
2025-09-06 20:05:53 +0530

    Time: 15 ms (42.23%), Space: 18 MB (34.06%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,16 +2,17 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        # count the frequency of characters in both strings and comapre the hashmaps
 
-        charFreq = {}       
-        
+        freqMap = {}
         for i in range(len(s)):
-            charFreq[s[i]] = charFreq.get(s[i],0) + 1
-            charFreq[t[i]] = charFreq.get(t[i],0) - 1
+            freqMap[s[i]] = freqMap.get(s[i],0) + 1
+            freqMap[t[i]] = freqMap.get(t[i],0) - 1
 
-        for val in charFreq.values():
-            if val != 0 :
+        for v in freqMap.values():
+            if v != 0:
                 return False
 
         return True            
+
+
+        
