bb8fa62356d5f2572693216d0561847594d2678b
Irtiqa Malik <Irtiqa.m02@gmail.com>
2025-09-15 20:13:14 +0530

    Time: 3 ms (81.09%), Space: 17.7 MB (69.53%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -2,18 +2,22 @@ class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         rows = defaultdict(set)
         cols = defaultdict(set)
-        squares = defaultdict(set) # key = (r//3, c//3)
+        squares = defaultdict(set)
 
         for r in range(9):
             for c in range(9):
                 if board[r][c] == '.':
                     continue
-                if( board[r][c] in rows[r] or
+                if (board[r][c] in rows[r] or
                     board[r][c] in cols[c] or
-                    board[r][c] in squares[(r//3,c//3)]):
+                    board[r][c] in squares[(r//3, c//3)]):
                     return False
+                # update the hashmap with the value seen
+
                 rows[r].add(board[r][c])
-                cols[c].add(board[r][c]) 
-                squares[(r//3, c//3)].add(board[r][c])  
-        return True             
+                cols[c].add(board[r][c])
+                squares[(r//3, c//3)].add(board[r][c])
+
+        return True              
+
         
