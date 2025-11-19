2145b146e5f0e2f627d7039e1e1eb6c412478bd7
Irtiqa Malik <Irtiqa.m02@gmail.com>
2025-11-19 23:59:41 +0530

    Time: 115 ms (73.45%), Space: 18 MB (48.96%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -4,11 +4,16 @@ class Solution:
         charMap = {}
         maxLength = 0
 
+        maxFreq = 0
+
         for r in range(len(s)):
             charMap[s[r]] = 1 + charMap.get(s[r],0) 
+            maxFreq = max(maxFreq,charMap[s[r]])
 
             #check if window is valid
-            while (r-l+1) - max(charMap.values()) > k:
+            #while (r-l+1) - max(charMap.values()) > k:
+            while (r-l+1) - maxFreq > k:
+    
                 charMap[s[l]] -= 1 
                 l += 1
 
