dfcfeea98cff1fc1ac926a4b3d57e010183173a7
Irtiqa Malik <Irtiqa.m02@gmail.com>
2026-02-08 22:11:29 +0530

    Time: 3 ms (79.07%), Space: 19.4 MB (22.26%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,20 +1,23 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = defaultdict(set)
-        cols = defaultdict(set)
-        squares = defaultdict(set) # key = r//3,c//3
+        rowmap = defaultdict(set) # key is row index
+        colmap = defaultdict(set)
+        sqmap = defaultdict(set) # key is r,c tuple // 3
 
         for r in range(9):
             for c in range(9):
                 if board[r][c] == '.':
                     continue
-                if (board[r][c] in rows[r] or
-                    board[r][c] in cols[c] or
-                    board[r][c] in squares[(r//3,c//3)]):
-                    return False
-                rows[r].add(board[r][c])
-                cols[c].add(board[r][c])
-                squares[(r//3,c//3)].add(board[r][c])  
+                if (board[r][c] in rowmap[r] or
+                    board[r][c] in colmap[c] or
+                    board[r][c] in sqmap[(r//3,c//3)]):
+                    return False; 
 
-        return True             
+                rowmap[r].add(board[r][c])
+                colmap[c].add(board[r][c])
+                sqmap[(r//3,c//3)].add(board[r][c])
+
+        return True        
+
+
         
