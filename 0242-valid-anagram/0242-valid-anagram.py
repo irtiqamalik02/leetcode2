06c73fd9bcdca887668f2b4f52a51e3bb02674c3
Irtiqa Malik <Irtiqa.m02@gmail.com>
2026-04-08 09:32:25 +0530

    Time: 6 ms (92.46%), Space: 19.6 MB (25.5%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,17 +1,4 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-        freqCounter = {}
-        for i in range(len(s)):
-            freqCounter[s[i]] = freqCounter.get(s[i],0) + 1
-            freqCounter[t[i]] = freqCounter.get(t[i],0) - 1
-
-        for n in freqCounter.values():
-            if n != 0:
-                return False
-
-        return True            
-
-
+        return Counter(s) == Counter(t)
         
