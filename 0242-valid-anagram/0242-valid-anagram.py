7d584b6cd8416bbe7037b0f32008cb8baae54897
Irtiqa Malik <Irtiqa.m02@gmail.com>
2026-04-09 07:30:49 +0530

    Time: 15 ms (43.11%), Space: 19.3 MB (96.35%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,9 +2,7 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-
         freqMap = {}
-
         for i in range(len(s)):
             freqMap[s[i]] = freqMap.get(s[i],0) + 1
             freqMap[t[i]] = freqMap.get(t[i],0) - 1
@@ -13,7 +11,5 @@ class Solution:
             if val != 0:
                 return False
 
-        return True            
-
-
+        return True                
         
