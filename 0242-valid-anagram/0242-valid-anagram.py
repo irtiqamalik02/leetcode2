a7efe41819df93e7da78feac05122e64ebf79b4b
Irtiqa Malik <Irtiqa.m02@gmail.com>
2026-06-11 09:18:20 +0530

    Time: 11 ms (76.37%), Space: 19.5 MB (27.69%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,11 +5,11 @@ class Solution:
         freqCounter = {}
         for i in range(len(s)):
             freqCounter[s[i]] = freqCounter.get(s[i],0) + 1
-            freqCounter[t[i]] = freqCounter.get(t[i],0) - 1 
+            freqCounter[t[i]] = freqCounter.get(t[i],0) - 1
 
         for val in freqCounter.values():
             if val != 0:
                 return False
 
-        return True                 
+        return True
         
