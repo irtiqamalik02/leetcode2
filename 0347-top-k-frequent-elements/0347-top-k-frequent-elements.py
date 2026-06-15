ca969226b1d522ffaf7f4ac4ba55ec8a3d8d8831
Irtiqa Malik <Irtiqa.m02@gmail.com>
2026-06-15 15:22:33 +0530

    Time: 8 ms (33.03%), Space: 24.9 MB (12.46%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,12 +1,21 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
         freqMap = Counter(nums)
-        minHeap = []
+        buckets = [[] for _ in range(len(nums)+1)]
 
         for num,freq in freqMap.items():
-            heapq.heappush(minHeap,(freq,num))
-            if len(minHeap) > k:
-                heapq.heappop(minHeap)
+            buckets[freq].append(num)
 
-        return [num for freq,num in minHeap]        
+        # try to retrive k elements from it by traversing in reverse order
+        res = []
+        for i in range(len(buckets)-1,0,-1):
+            for n in buckets[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+
+
+
+
         
